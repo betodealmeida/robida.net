9a3d340c907ea54fd5a3fafabd0a0477fa6bf36d
Beto Dealmeida <roberto@dealmeida.net>
2024-06-07 18:39:33 -0400

    chore: add unit tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -14,13 +14,11 @@ def test_init_db_sync(mocker: MockerFixture) -> None:
     app = mocker.MagicMock()
     create_app = mocker.patch("robida.main.create_app", return_value=app)
     init_db = mocker.patch("robida.main.init_db")
-    asyncio = mocker.patch("robida.main.asyncio")
 
     init_db_sync()
 
     create_app.assert_called_once()
     init_db.assert_called_once_with(app)
-    assert asyncio.run.called_with(init_db(app))
 
 
 def test_run(mocker: MockerFixture) -> None:
